84e2c1f8ccf778daf0d4f9a53596b16a182f94e3
Mikhail Makarov <mikhail.makarov@phystech.edu>
2023-10-25 13:51:26 +0300

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main >
+    Accelerometer
+    <ul>
+      <li v-for="(item, index) in rawData">
+        {{ item }}
+      </li>
+    </ul>
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref, type Ref, onMounted, computed } from 'vue';
+
+type Item = {
+  x: number | null;
+  y: number | null;
+  z: number | null;
+  t: number | null;
+}
+
+const rawData: Ref<Item[]> = ref([]);
+
+onMounted(async () => {
+  window.addEventListener('devicemotion', (event: DeviceMotionEvent) => {
+    if (event.acceleration) {
+      rawData.value.push({
+        x: event.acceleration.x,
+        y: event.acceleration.y,
+        z: event.acceleration.z,
+        t: event.interval,
+      })
+    }
+  });
+});
+
+</script>
