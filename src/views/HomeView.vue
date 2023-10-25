61a40096c0ac6c78c04547c4f2df99615d53232e
Mikhail Makarov <mikhail.makarov@phystech.edu>
2023-10-25 17:07:53 +0300

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,16 @@
       <span style="display: block;">Max z: {{ rawData.z }}   m/s<sup>2</sup></span>
       <span style="display: block;">t: {{ rawData.t }}   ms</span>
     </article>
+
+    <div>
+      <h2>Chart</h2>
+      <svg></svg>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
+ import * as d3 from 'd3';
 import { ref, type Ref, onMounted } from 'vue';
 
 type Item = {
@@ -24,8 +30,11 @@ const rawData: Ref<Item> = ref({
   x: 0, y: 0, z: 0, t: 0
 });
 
+const accData: Ref<Item[]> = ref([]);
+const iteration = ref(0);
 const setHandler = () => {
   window.addEventListener('devicemotion', (event: DeviceMotionEvent) => {
+    iteration.value++;
       if (event.acceleration) {
         const { x, y, z } = event.acceleration;
         if (x !== null && y !== null && z !== null) {
@@ -34,6 +43,17 @@ const setHandler = () => {
           rawData.value.z = rawData.value.z < z ? z : rawData.value.z
         }
         rawData.value.t = event.interval;
+
+        if (accData.value.length > 1000) {
+          accData.value.shift()
+        }
+        if (accData.value.length <= 1000) {
+          
+          if (x !== null && y !== null && z !== null) {
+            accData.value.push({ t: iteration.value, x: x, y: y, z: z});
+          }
+        }
+
       }
     });
 }
@@ -52,5 +72,66 @@ onMounted(async () => {
     setHandler();
   }
 });
+
+onMounted(() => {
+  const width = 800;
+  const height = 500;
+  const data = accData.value;
+  const svg = d3.select("svg").attr("width", width).attr("height", height);
+  const g = svg.append("g");
+
+  //3. Creating the Chart Axes
+  const x = d3
+    .scaleTime()
+    .domain(
+      d3.extent(data, function (d: any) {
+        return d.t;
+      })
+    )
+    .rangeRound([0, width]);
+
+  const y = d3
+    .scaleLinear()
+    .domain(
+      d3.extent(data, function (d: any) {
+        return d.x;
+      })
+    )
+    .rangeRound([height, 0]);
+
+  //4. Creating a Line
+  const line = d3
+    .line()
+    .x(function (d: any) {
+      return x(d.t);
+    })
+    .y(function (d: any) {
+      return y(d.x);
+    });
+
+  //5. Appending the Axes to the Chart
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+
+  //6. Appending a path to the Chart
+  g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+});
+
+
+
 
 </script>
